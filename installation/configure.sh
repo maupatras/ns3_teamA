d4646a52705d1a4d6f5dd44f35d6c255ae7c7906
spsteve <sp_steve@hotmail.com>
2015-04-08 11:53:55 +0300

    Update configure.sh

--- installation/configure.sh
+++ installation/configure.sh
@@ -16,3 +16,14 @@ read version
 }
 
 insert_version
+
+prerequisites () {
+
+	echo ""
+	echo $blue"Installing Prerequisites..."$reset
+	echo ""
+	sudo apt-get install make python python-dev mercurial bzr gdb valgrind gsl-bin libgsl0-dev libgsl0ldbl flex bison tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick texlive texlive-latex-extra texlive-generic-extra texlive-generic-recommended texinfo dia texlive texlive-latex-extra texlive-extra-utils texlive-generic-recommended texi2html python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml 
+	sudo apt-get update
+	echo ""
+	echo $green"Prerequisites installed"$reset
+} 
