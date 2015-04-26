37e2552654b45add376b09327b8fddd86d55f97a
Mike Drakoulelis <mikedrakoulelis@yahoo.gr>
2015-04-26 15:47:39 +0300

    Various fixes
    Fixed unzipping and random folder input parameters

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 param ( 
-	$tempcygwindir="$env:temp\cygInstall",
+	[string]$tempcygwindir="$env:temp\cygInstall",
 	[string]$folder = $PSScriptRoot,
-	$cygwinpath = "C:\Cygwin\"
+	[string]$cygwinpath = "C:\Cygwin\"
 )
 Echo $tempcygwindir
 If (!(Test-Path -Path $tempcygwindir -PathType Container))
@@ -33,7 +33,7 @@ If ($os.OSArchitecture -Match "64-bit") {
 Write-Host "Download complete" -foreground "green"
 
 # Install Cygwin and packages
-Write-Host '`nIntalling Cygwin `& prerequisite packages...' -foreground "cyan"
+Write-Host "`nIntalling Cygwin `& prerequisite packages..." -foreground "cyan"
 Start-Process -wait -FilePath "$tempcygwindir\setup.exe" -ArgumentList "-q -n -l $tempcygwindir -s ftp://ftp.ntua.gr/pub/pc/cygwin/ -R $cygwinpath -P gcc-core,gcc-g++,make,gdb,libQtCore4-devel,libQtGui4-devel -C Python"
 $env:path = "$($env:path);$cygwinpath\bin"
 Write-Host "Done" -foreground "green"
@@ -42,29 +42,37 @@ Write-Host "Done" -foreground "green"
 Write-Host "`nDownloading NS-3 v3.22..." -foreground "cyan"
 Invoke-WebRequest -OutFile "$tempcygwindir\ns3.tar.bz2" https://www.nsnam.org/release/ns-allinone-3.22.tar.bz2
 
-Write-Host "Done. Unzipping files..." -foreground "green"
-tar.exe -xjf $tempcygwindir\ns3.tar.bz2 -C $folder
-Write-Host "`nDone" -foreground "green"
+Write-Host "Download complete. Unzipping files to $folder..." -foreground "green"
+If ($folder.EndsWith("\")) {
+	$s = $folder + "."
+	tar.exe -xjf $tempcygwindir\ns3.tar.bz2 -C $s --force-local
+} ElseIf (!$folder.EndsWith("\.")) {
+	$s = $folder + "\."
+	tar.exe -xjf $tempcygwindir\ns3.tar.bz2 -C $ --force-local
+} Else {
+	tar.exe -xjf $tempcygwindir\ns3.tar.bz2 -C $folder --force-local
+}
+Write-Host "Done" -foreground "green"
 
 # Install NS-3
-Write-Host "Building NS-3..." -foreground "cyan"
+Write-Host "`nBuilding NS-3..." -foreground "cyan"
 cd $folder\ns-allinone-3.22\
 
 New-Alias -Name python -Value "$cygwinpath\bin\python2.7.exe"
 python ./build.py --enable-examples --enable-tests
-Write-Host "`nBuilding complete" -foreground "green"
+Write-Host "Building complete" -foreground "green"
 
 #Install NetAnim
 If (!$nonam) {
-	Write-Host "Building NetAnim" -foreground "cyan"
+	Write-Host "`nBuilding NetAnim" -foreground "cyan"
 	cd $folder\ns-allinone-3.22\netanim-3.105
 	make clean
 	& $cygwinpath\lib\qt4\bin\qmake.exe NetAnim.pro
 	make
-	Write-Host "`nBuilding complete" -foreground "green"
+	Write-Host "Building complete" -foreground "green"
 }
 
 # Remove temp files
-Write-Host "All installations complete. Deleting temp download folder..." -foreground "cyan"
+Write-Host "`nAll installations complete. Deleting temporary download folder..." -foreground "cyan"
 rmdir $tempcygwindir -recurse
 Write-Host "Temporary folder $tempcygwindir successfully deleted" -foreground "green"
